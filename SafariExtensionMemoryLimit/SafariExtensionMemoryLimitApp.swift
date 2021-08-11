217463404450e7b2a6281dfa6e0a2d80c61e7195
Andrew Liakh <liakhandrii@gmail.com>
2021-08-11 09:30:07 +0200

    Initial Commit

---
+++ SafariExtensionMemoryLimit/SafariExtensionMemoryLimitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariExtensionMemoryLimitApp.swift
+//  SafariExtensionMemoryLimit
+//
+//  Created by Andrew Liakh on 11.08.21.
+//
+
+import SwiftUI
+
+@main
+struct SafariExtensionMemoryLimitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
